8011affd63ef37d5ea2aa94acd857cf5eaa4828c
shipatulhasan <shipatulhasan328@gmail.com>
2024-10-11 20:06:11 +0600

    made initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
+import { store } from './app/store.js'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ChakraProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+  </ChakraProvider>
 )
